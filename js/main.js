acba15e56bcf031674c436c1567a109383bdaa06
Bramus <bramus@bram.us>
2012-02-14 17:27:34 +0100

    HTML Example can also be run now (shown in overlay)

--- js/main.js
+++ js/main.js
@@ -138,13 +138,110 @@ window.addEventListener('load', function() {
 		new Incrementable(cssBlock);
 		
 	}
+
+	// Add Show in Overlay Button to HTML Blocks
+	var htmlBlocks = document.querySelectorAll('code.language-html');
+	for (var i = 0, len = htmlBlocks.length; i < len ; i++) {
+
+		// Local reference
+		var htmlBlock = htmlBlocks[i];
+
+		// may we add the run button to it?
+		if (htmlBlock.className.match(new RegExp('(\\s|^)dontrun(\\s|$)'))) continue;
+
+		// Add Run button
+		var button = document.createElement('input');
+		button.type = 'submit';
+		button.value = 'Show in overlay';
+		button.className = 'run';
+		button.addEventListener('click', function() {
+			var codeBlock = this.parentNode.querySelector('code');
+			showInOverlay(codeBlock, htmlentities(stripHTML(codeBlock.innerHTML)));
+		});
+		htmlBlock.parentNode.appendChild(button);
+
+		// Hook ctrl+enter to run the code
+		htmlBlock.addEventListener('keypress', function(evt) {
+			if(evt.ctrlKey && (evt.keyCode == 13)) {
+				evt.preventDefault();
+				evt.stopPropagation();
+				showInOverlay(this, this.innerHTML);
+			}
+		}, false);
+
+	}
+	
+	// show a blob of HTML inside an overlay
+	var showInOverlay = function(codeBlock, html, width, height) {
+		
+		codeBlock.blur();
+		
+		var height = parseInt(height || 460, 10);
+		var width = parseInt(width || 680, 10);
+		
+		// Make sure overlay UI elements are present
+		if (!document.getElementById('overlayBack')) {
+			var overlayBack = document.createElement('div');
+			overlayBack.id = "overlayBack";
+			overlayBack.addEventListener('click', function(e) {
+				e.stopPropagation();
+				
+				// erase contents (to prevent movies from playing int he back for example)
+				var oIframe = document.getElementById('daframe');
+				var oDoc = (oIframe.contentWindow || oIframe.contentDocument);
+				if (oDoc.document) oDoc = oDoc.document;
+				oDoc.write('');
+				
+				// hide
+				document.getElementById('overlayBack').style.display = 'none';
+				document.getElementById('overlayContent').style.display = 'none';
+			});
+			document.body.appendChild(overlayBack);
+		}
+
+		if (!document.getElementById('overlayContent')) {
+			var overlayContent = document.createElement('div');
+			overlayContent.id = "overlayContent";
+			document.body.appendChild(overlayContent);
+		}
+		
+		// reference elements we'll be needing
+		var ob = document.getElementById('overlayBack');
+		var oc = document.getElementById('overlayContent');
+		
+		// Position oc
+		oc.style.width = width + 'px';
+		oc.style.height = height + 'px';
+		oc.style.marginTop = '-' + (height/2) + 'px';
+		oc.style.marginLeft = '-' + (width/2) + 'px';
+		
+		// inject iframe
+		oc.innerHTML = '<iframe src="assets/blank.html" height=""' + height + '" width="' + width + '" border="0" id="daframe"></iframe>';
+		
+		// inject HTML
+		var oIframe = document.getElementById('daframe');
+		var oDoc = (oIframe.contentWindow || oIframe.contentDocument);
+		if (oDoc.document) oDoc = oDoc.document;
+		oDoc.write(html_entity_decode(html));
+		
+		// Show me the money!
+		ob.style.display = 'block';
+		oc.style.display = 'block';
+		
+	}
 	
 	// Print fix (Firefox, IE only though)
 	window.addEventListener('beforeprint', function(e) {
+		
+		// unhide all slides
 		var els = document.querySelectorAll('#reveal .slides > section');
 		for(var i = 0, len = els.length; i < len; i++) {
 			els[i].style.display = 'block';
 		}
+		
+		// hide overlay
+		document.getElementById('overlayBack').style.display = 'none';
+		document.getElementById('overlayContent').style.display = 'none';
 	});
 	
 }, false);
